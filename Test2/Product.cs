02bb12ef3a42ef63a24a16bda4e3b49ade2b400a
Кристина Гвоздюкевич <honey_kris@outlook.com>
2020-04-25 13:25:49 +0300

    Add project files.

---
+++ Test2/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test2
+{
+    public class Product : IComparable
+    {
+        public string Id { get; set; }
+        public string Tree_name { get; set; }
+
+        public Product(string id, string tree_Name)
+        {
+            Id = id;
+            Tree_name = tree_Name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Test2
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("myList.json");
+            List<Product> MyList = JsonConvert.DeserializeObject<List<Product>>(json);
+            var sortedList = MyList.OrderBy(x => x.Id, new MyListComparer()).ToList();
+
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine($"{item.Id} {item.Tree_name}");
+            }
+
+        }
+
+    }
+}
